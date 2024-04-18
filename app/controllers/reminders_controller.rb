452329910f9f8040edf4a2633acfda9760193f54
Arthur Borges <arthurborges.dev@gmail.com>
2024-04-18 06:56:21 -0300

    feat: Add show and edit actions and views plus form partial

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -3,6 +3,40 @@ class RemindersController < ApplicationController
     @reminders = Reminder.all
   end
 
-  def new
+  def show
+    @reminder = Reminder.find(params[:id])
   end
+  
+  def new
+    @reminder = Reminder.new
+  end
+
+  def create
+    @reminder = Reminder.new(reminder_params)
+
+    if @reminder.save
+      redirect_to @reminder
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @reminder = Reminder.find(params[:id])
+  end
+
+  def update
+    @reminder = Reminder.find(params[:id])
+
+    if @reminder.update(reminder_params)
+      redirect_to @reminder
+    else
+      render :edit, status: unprocessable_entity
+    end
+  end
+
+  private
+    def reminder_params
+      params.require(:reminder).permit(:url, :page_number)
+    end
 end
--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,3 +1,4 @@
 class Reminder < ApplicationRecord
   validates :url, presence: true
+  validates :page_number, presence: true, numericality: { only_integer: true }
 end
