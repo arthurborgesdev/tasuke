b54825a4e04f11b6f75c543b4ff6bd5cba6ce055
Arthur Borges <arthurborges.dev@gmail.com>
2024-04-29 21:25:56 -0300

    feat: add migration step to script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,5 +3,9 @@
 set -o errexit
 
 bundle install
+
+# Run database migrations
+bundle exec rails db:migrate RAILS_ENV=production
+
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
